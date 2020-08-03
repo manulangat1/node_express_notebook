9930b1d9f257a6291ecfdc2516d53de2207a5571
manulangat <emmanuelthedeveloper@gmail.com>
2020-08-03 18:55:21 +0300

    ft:adding_notes_list:backend

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,21 +1,42 @@
 // use model to get data 
-
+const Notes = require('../models/Notes')
 // #desc GET all transactions
 // @route GET /api/v1/transactions/
 // @access PUBLIC
-exports.getNotes = (req,res,next) => {
-    res.send("GET TRANSACTIONS")
+exports.getNotes = async (req,res,next) => {
+    try{
+        const notes = await Notes.find()
+        return res.status(200).json({
+            success:true,
+            count:notes.length,
+            data:notes
+        })
+    } catch (err){
+        console.log(`error:${err}`)
+    }
+    
+    // res.send("GET TRANSACTIONS")
 }
 
 //@desc POST transaction 
 // @route POST /api/v1/transactions/
 // @access PUBLIC
-exports.postNote = (req,res,next) => {
+exports.postNote = async (req,res,next) => {
+    // try{
+
+    // } catch (e){
+
+    // }
     res.send("POST TRANSACTIONS")
 }
 // @desc DELETE transaction
 // @route DELETE /api/v1/transactions/:id
 // @access PUBLIC
-exports.delNote = (req,res,next) => {
+exports.delNote = async (req,res,next) => {
+    // try{
+
+    // } catch (e){
+
+    // }
     res.send("DELETE TRANSACTIONS")
 }
